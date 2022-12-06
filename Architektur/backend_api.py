3e81945049ba7c7c8aed129398ac4b99daedc5c6
Marcus02W <marcuswirth.02@gmail.com>
2022-12-06 10:17:53 +0100

    FER NN integrated in architecture, changed keras load_img to PIL image preprocessing in order to not have to save uploaded pictures on the server

--- Architektur/backend_api.py
+++ Architektur/backend_api.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
-from tensorflow import keras
 
-model = keras.models.load_model('./fer_model.h5')
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+from keras.preprocessing import image
+
+from PIL import Image, ImageOps
+
+
+
+model = keras.models.load_model('../Backend/Bild KI/ferNet.h5')
+# model from https://www.kaggle.com/code/anantgupt/facial-emotion-recogination-91-accuracy-train-set/notebook
+
+
+
 
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 
@@ -17,8 +30,18 @@ def loadFrontend():
 def generate_output():
     
     form_data = request.form
-    #user_image = request.files['image']
+    user_image = request.files['image']
+
+    img = Image.open(user_image)
+    img2 = ImageOps.grayscale(img)
+    img3 = img2.resize((48,48))
+    img_array = tf.keras.utils.img_to_array(img3)
+    img_batch = np.expand_dims(img_array,axis = 0)
+
+    prediction = model.predict(img_batch)
+    mapping_list = ['angry','disgust','fear','happy','neutral','sad','surprise']
+    result = mapping_list[prediction[0].argmax()]
 
     # banking_product = NeuralNetwork.calc_banking_product(user_image)   # So könnte das später aussehen
 
-    return form_data['job']
+    return result
