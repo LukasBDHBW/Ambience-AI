d3acea2a45ba55a10c1287e6d54ce111813f707c
Marcus02W <marcuswirth.02@gmail.com>
2023-01-05 01:36:59 +0100

    frontend design of first view implemented

--- Architektur/staticFiles/Main.js
+++ Architektur/staticFiles/Main.js
@@ -1,3 +1,5 @@
+
+
 // http handling
 
 function send_data(){
@@ -29,16 +31,28 @@ function send_data(){
 // camera input handling
 
 async function start_camera () {
+    // DOM manipulation part
+    document.getElementById("first_view").style.display="None";
+    document.getElementById("second_view").style.display="inline";
+
+
+    // ================================== //
+    // functional part
    	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
 	document.getElementById("video").srcObject = stream;
 };
 
 function take_picture() {
 
+
+
    	document.getElementById("canvas").getContext('2d').drawImage(document.getElementById("video"), 0, 0, document.getElementById("canvas").width, document.getElementById("canvas").height);
  
+
+}
+
+function send_img() {
     let image_base64 = document.querySelector("#canvas").toDataURL('image/jpeg').replace(/^data:image\/jpeg;base64,/, ""); 
-    //console.log(image_base64)  
     var xhr = new XMLHttpRequest(),
         data = image_base64;
 
@@ -53,10 +67,5 @@ function take_picture() {
             document.getElementById("emotion_response").innerHTML=response;
         }
     }
-        
-        
-
-      
-    
-
-};
+}
+;
